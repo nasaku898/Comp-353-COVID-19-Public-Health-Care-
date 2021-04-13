ff4880e2a141b99a63d03014e32654a84a1ac729
Manraj Rai <manraj14@icloud.com>
2021-04-13 10:33:41 -0400

    Updated index page

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
-<?php require_once './db/db_connection.php';
-$statement = $conn->prepare('SELECT * FROM aec353_4.person');
-$statement->execute();
-?>
+<?php require_once './db/db_connection.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
-<head> 
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Covid Health Application</title>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Welcome</title>
 </head>
+
 <body>
-    <h1>Hello World</h1>
-    <?php while($row = $statement->fetch(PDO::FETCH_ASSOC,PDO::FETCH_ORI_NEXT)){?>
-        <h1><?= $row["firstName"] ?></h1>
-    <?php } ?>
+    <h1 class="title">Welcome</h1>
+    <div class="button">
+        <button type="button" class="specificButton">Patient</button>
+        <button type="button" class="specificButton">Worker</button>
+        <button type="button" class="specificButton">Admin</button>
+    </div>
 </body>
+
 </html>
