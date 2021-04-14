8cf72c05ebf5eb68d8e84f13265bb4c052740556
Simon Lim <simonlim898@hotmail.com>
2021-04-14 17:43:25 -0400

    updated pages

--- patient-associateChild.php
+++ patient-associateChild.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="patient-registration.css">
-    <title>Patient Register</title>
+    <title>Patient Associate Child</title>
 </head>
 
 <body>
--- patient-index.php
+++ patient-index.php
@@ -6,11 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-    <title>Welcome</title>
+    <title>Welcome Patient</title>
 </head>
 
 <body>
-    <h1 class="title">Welcome</h1>
+    <h1 class="title">Welcome Patient</h1>
     <div class="button">
         <a href="/patient-register.php">
             <button type="button" class="specificButton">
