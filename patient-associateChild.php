a9fd5b5ff0fcf895f0c46841583415da8c44f7e1
Simon Lim <simonlim898@hotmail.com>
2021-04-14 14:11:42 -0400

    Implement create parent of relation

---
+++ patient-associateChild.php
@@ -0,0 +1,50 @@
+<?php
+require_once 'db/db_connection.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $parentMedicare = $_POST["parentMedicare"];
+    $childMedicare = $_POST["childMedicare"];
+
+    if (empty(trim($parentMedicare)) || empty(trim($childMedicare))) {
+        $error = "error";
+        echo $error;
+    } else {
+        $createParentOf = $conn->prepare("INSERT INTO parentOf(parentId, childId) VALUES(:parentMedicare,:childMedicare)");
+        $createParentOf->bindParam(":parentMedicare", $parentMedicare);
+        $createParentOf->bindParam(":childMedicare", $childMedicare);
+
+        if ($createParentOf->execute()) {
+            unset($_POST);
+            ob_start();
+            header("location: https://aec353.encs.concordia.ca/admin-home.php");
+            ob_end_flush();
+            die();
+        }
+    }
+    unset($_POST);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="patient-registration.css">
+    <title>Patient Register</title>
+</head>
+
+<body>
+    <h1 class="title">Associate Child</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="parentMedicare">Parent Medicare</label>
+        <input type="text" id="parentMedicare" name="parentMedicare">
+        <br />
+        <label for="childMedicare">Child Medicare</label>
+        <input type="text" id="childMedicare" name="childMedicare">
+        <br />
+        <p>**Make sure to create your child first**</p>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
