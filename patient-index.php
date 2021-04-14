d637339c0bf15c7760aef3ed56c96b0aabc4312a
Simon Lim <simonlim898@hotmail.com>
2021-04-14 15:47:08 -0400

    implemented patient login, index and home

---
+++ patient-index.php
@@ -0,0 +1,26 @@
+<?php require_once './db/db_connection.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Welcome</title>
+</head>
+
+<body>
+    <h1 class="title">Welcome</h1>
+    <div class="button">
+        <a href="/patient-register.php">
+            <button type="button" class="specificButton">
+                <a href="/patient-register.php">
+                    Create
+                </a>
+            </button>
+
+            <button type="button" class="specificButton">Login</button>
+    </div>
+</body>
+
+</html>
