b9337287d130f8a77922de3482fc0715b86ef2a9
Manraj Rai <manraj14@icloud.com>
2021-04-15 17:34:27 -0400

    Implemented guarding for admin home page

--- admin-home.php
+++ admin-home.php
@@ -1,3 +1,15 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    ob_start();
+    header("location: https://aec353.encs.concordia.ca/admin-login.php");
+    ob_end_flush();
+    die();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
