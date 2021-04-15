8cc0d6c64f0b2827c74f68fc688c71c8cc0438f0
Simon Lim <simonlim898@hotmail.com>
2021-04-15 17:51:48 -0400

    implemented create region

--- admin-HomeButton.php
+++ admin-HomeButton.php
@@ -8,21 +8,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     die();
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin-HomeButton.css">
-</head>
-
-<body>
-    <div class="homeButtonDiv">
-        <a href="https://aec353.encs.concordia.ca/admin-home.php">
-            <button type="button" id="homeButton">Home</button>
-        </a>
-    </div>
-</body>
-
-</html>
+<div class="homeButtonDiv">
+    <a href="https://aec353.encs.concordia.ca/admin-home.php">
+        <button type="button" id="homeButton">Home</button>
+    </a>
+</div>
